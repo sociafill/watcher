d429e21196f728746dca5a9427cad89dfa2eefbb
Johnny Debt <38006769+johnny-debt@users.noreply.github.com>
2018-07-31 00:15:58 +0300

    Docker build with GitLab CI

--- .gitlab/init.sh
+++ .gitlab/init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Initializes project directory for build / test / etc...
 go get -u github.com/golang/dep/cmd/dep
-mkdir -p $GOPATH/src
-cd $GOPATH/src
-ln -s $CI_PROJECT_DIR
-cd $CI_PROJECT_NAME
 dep ensure
